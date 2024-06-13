bbacba806e5044b1cedc70a3a80ffc766e2588b4
Praveen <Praveensonu1999@gmail.com>
2024-06-13 11:55:50 +0530

    Update From Sun

--- src/main/java/com/practice/relatime/controller/PracticeController.java
+++ src/main/java/com/practice/relatime/controller/PracticeController.java
@@ -3,6 +3,8 @@ package com.practice.relatime.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import groovy.util.logging.Slf4j;
+
 @RequestMapping
 @Slf4j
 public class PracticeController {
