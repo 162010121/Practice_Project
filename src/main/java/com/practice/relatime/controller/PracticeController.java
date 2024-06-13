f560c65ea56542ef78b06757a0aa1eec537a0da8
Praveen <Praveensonu1999@gmail.com>
2024-06-13 11:38:56 +0530

    PracticeProject

---
+++ src/main/java/com/practice/relatime/controller/PracticeController.java
@@ -0,0 +1,16 @@
+package com.practice.relatime.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+public class PracticeController {
+
+	@GetMapping("/getMessage")
+	public String getMessage()
+	{
+		System.out.println("Hi This Message From Spring Boot Practice Project");
+
+		return "Hi Message From Spring Boot";
+	}
+}
